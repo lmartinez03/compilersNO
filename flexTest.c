a297a4ba07e8107248be8efff30cdc7aa87a0b5a
Luca <lucamartinez03@gmail.com>
2025-03-31 17:58:43 +0000

    all

--- flexTest.c
+++ flexTest.c
@@ -1,6 +1,6 @@
-#line 2 "scan.c"
+#line 2 "flexTest.c"
 
-#line 4 "scan.c"
+#line 4 "flexTest.c"
 
 #define  YY_INT_ALIGNED short int
 
@@ -352,8 +352,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 1
-#define YY_END_OF_BUFFER 2
+#define YY_NUM_RULES 2
+#define YY_END_OF_BUFFER 3
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -361,14 +361,14 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[6] =
+static const flex_int16_t yy_accept[9] =
     {   0,
-        0,    0,    2,    1,    0
+        0,    0,    3,    2,    1,    0,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -398,29 +398,29 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[2] =
+static const YY_CHAR yy_meta[3] =
     {   0,
-        1
+        1,    1
     } ;
 
-static const flex_int16_t yy_base[7] =
+static const flex_int16_t yy_base[9] =
     {   0,
-        0,    0,    2,    3,    3,    0
+        0,    0,    5,    2,    6,    0,    6,    6
     } ;
 
-static const flex_int16_t yy_def[7] =
+static const flex_int16_t yy_def[9] =
     {   0,
-        6,    6,    5,    5,    0,    5
+        8,    1,    8,    8,    8,    4,    8,    0
     } ;
 
-static const flex_int16_t yy_nxt[5] =
+static const flex_int16_t yy_nxt[9] =
     {   0,
-        4,    5,    3,    5
+        4,    5,    6,    7,    8,    3,    8,    8
     } ;
 
-static const flex_int16_t yy_chk[5] =
+static const flex_int16_t yy_chk[9] =
     {   0,
-        6,    3,    5,    5
+        1,    1,    4,    4,    3,    8,    8,    8
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -437,12 +437,15 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "tiny.l"
-/****************************************************/
-/* File: tiny.l                                     */
-/* Spring 2025                                      */
-/****************************************************/
-#line 446 "scan.c"
+#line 1 "flexTest.l"
+#line 2 "flexTest.l"
+/* a text program that adds line #s to lines of texts, printing the new text 
+   to the standard output   
+*/
+#include <stdio.h>
+int lineno = 1;
+#line 448 "flexTest.c"
+#line 449 "flexTest.c"
 
 #define INITIAL 0
 
@@ -659,12 +662,9 @@ YY_DECL
 		}
 
 	{
-#line 7 "tiny.l"
+#line 9 "flexTest.l"
 
-
-
-
-#line 668 "scan.c"
+#line 668 "flexTest.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -691,13 +691,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 6 )
+				if ( yy_current_state >= 9 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 3 );
+		while ( yy_base[yy_current_state] != 6 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -722,11 +722,17 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
+/* rule 1 can match eol */
 YY_RULE_SETUP
-#line 11 "tiny.l"
+#line 10 "flexTest.l"
+{printf("%5d %s ", lineno++, yytext); }
+	YY_BREAK
+case 2:
+YY_RULE_SETUP
+#line 11 "flexTest.l"
 ECHO;
 	YY_BREAK
-#line 730 "scan.c"
+#line 736 "flexTest.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1023,7 +1029,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 6 )
+			if ( yy_current_state >= 9 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1051,11 +1057,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 6 )
+		if ( yy_current_state >= 9 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 5);
+	yy_is_jam = (yy_current_state == 8);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1731,25 +1737,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 11 "tiny.l"
+#line 11 "flexTest.l"
 
-
-TokenType getToken(void)
-{ static int firstTime = TRUE;
-  TokenType currentToken;
-  if (firstTime)
-  { firstTime = FALSE;
-    lineno++;
-    yyin = source;
-    yyout = listing;
-  }
-  currentToken = yylex();
-  strncpy(tokenString,yytext,MAXTOKENLEN);
-  if (TraceScan) {
-    fprintf(listing,"\t%d: ",lineno);
-    printToken(currentToken,tokenString);
-  }
-  return currentToken;
-}
-
-
+main()
+{yylex; return 0;}
